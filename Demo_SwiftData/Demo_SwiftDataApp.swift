2d8051854366e81721e6a7bc714095177171437e
travis556class <tmontgomery1@mail.sfsu.edu>
2025-11-12 16:22:03 -0800

    Initial Commit

---
+++ Demo_SwiftData/Demo_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftDataApp.swift
+//  Demo_SwiftData
+//
+//  Created by Travis Earl Montgomery on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
