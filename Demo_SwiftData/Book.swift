a67c0df5bb4c3fcb88ef2dc2f2478858c11b1463
travis556class <tmontgomery1@mail.sfsu.edu>
2025-11-12 16:40:52 -0800

    Setup Book model & prepared UI to process Book entries

---
+++ Demo_SwiftData/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  Demo_SwiftData
+//
+//  Created by Travis Earl Montgomery on 11/12/25.
+//
+
+import Foundation
+
+struct Book {
+    var title: String
+    var author: String
+    var year: Int
+}
--- Demo_SwiftData/ContentView.swift
+++ Demo_SwiftData/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK - Properties
+    @State private var title = ""
+    @State private var author = ""
+    @State private var bookYear = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Enter a new Book")
+                    .font(.largeTitle)
+                    .foregroundStyle(.orange)
+                    .padding()
+                
+                HStack {
+                    Text("Book Title: ")
+                        .font(.headline)
+                        .padding(5)
+                    TextField("Enter a Title", text: $title)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                HStack {
+                    Text("Book Author: ")
+                        .font(.headline)
+                        .padding(5)
+                    TextField("Author of Book", text: $author)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                HStack {
+                    Text("Publication Year: ")
+                        .font(.headline)
+                        .padding(5)
+                    TextField("Author of Book", text: $bookYear)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Button("Add Book") {
+                        let book: Book
+                        
+                        if let bookYear = Int(bookYear) {
+                            //success
+                            book = Book(title: title, author: author, year: bookYear)
+                        } else {
+                            book = Book(title: title, author: author, year: 0)
+                        }
+                    }
+                    .bold()
+                    .buttonStyle(.borderedProminent)
+                }
+                
+                //modelContext.insert(book)
+            }
         }
-        .padding()
     }
 }
 
